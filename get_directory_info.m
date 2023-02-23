503caa42c13d0d112104c20426b4e215691fbc68
Jonathan Gould <jonathan.d.gould@gmail.com>
2023-02-23 14:35:52 -0500

    updates paths to reflect a directory name change

--- get_directory_info.m
+++ get_directory_info.m
@@ -4,7 +4,7 @@ function directory_info = get_directory_info()
 % are downloaded onto a new computer), these folders should be changed.
 
     directory_info.chronux_folder = '/Users/jonathangould/Documents/MATLAB/chronux_2_11';
-    directory_info.data_folder = '/Users/jonathangould/Dropbox (University of Michigan)/Winter_2022/rotation_parent/spectral_analysis/data_folder/';
-    directory_info.output_folder = '/Users/jonathangould/Dropbox (University of Michigan)/Winter_2022/rotation_parent/spectral_analysis/generated/';
+    directory_info.data_folder = '/Users/jonathangould/Dropbox (University of Michigan)/Winter_2022/rotation_parent/pcdh19_eeg_analysis/data_folder';
+    directory_info.output_folder = '/Users/jonathangould/Dropbox (University of Michigan)/Winter_2022/rotation_parent/pcdh19_eeg_analysis/generated';
 end
 
